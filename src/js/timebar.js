3cf42e97aef6dc225d41366765f8be196f2b7617
pulkitchdha <pulkitchadha27@gmail.com>
2018-03-06 16:43:44 +0530

    add events

---
+++ src/js/timebar.js
@@ -0,0 +1,258 @@
+/**
+ * Author: Pulkit Chadha
+ * Plugin Name: Time Bar 
+ * 
+*/
+(function ($) {
+	$.fn.timebar = function (options = {}) {
+		const self = this;
+
+		options = $.extend($.fn.timebar.defaults, options);
+
+		$.fn.timebar.defaults.element = self;
+
+		// methods
+		this.getSelectedTime = function () {
+			return $.fn.timebar.defaults.selectedTime;
+		};
+		this.setSelectedTime = function (time) {
+			$.fn.timebar.defaults.selectedTime = time;
+			return this;
+		};
+		this.getTotalTime = function () {
+			return $.fn.timebar.defaults.totalTime;
+		};
+		this.setTotalTime = function (time) {
+			$.fn.timebar.defaults.totalTime = time;
+			return this;
+		};
+		this.getWidth = function () {
+			return $.fn.timebar.defaults.width;
+		};
+		this.setWidth = function (width) {
+			$.fn.timebar.defaults.width = width;
+			$(".timeline-cover").css('width', width);
+			return this;
+		};
+		this.getActualWidth = function () {
+			const width = $.fn.timebar.defaults.width;
+			return width.replace(/px|%/g, '');
+		}
+		this.getCuepoints = function () {
+			return $.fn.timebar.defaults.cuePoints;
+		}
+		this.addCuepoints = function (cuepoint) {
+			$.fn.timebar.defaults.cuepoints.push(cuepoint);
+			return this;
+		}
+		this.deleteCuepoints = function (id) {
+			const cuepoints = $.fn.timebar.defaults.cuepoints;
+			for (let i = 0; i < cuePoints.length; i++) {
+				if (cuePoints[i] == id) {
+					cuePoints.splice(i, 1);
+				}
+			}
+		}
+		this.showCuepoints = function () {
+			$(".pointer").show();
+		}
+		this.hideCuepoints = function () {
+			$(".pointer").hide();
+		}
+
+		// events
+
+		return self.each(function () {
+			init(self);
+
+			$(this).on('click', '.steps-bar', function (event) {
+				const selectedTime = barClicked(this, event, self);
+
+				if ($.isFunction(options.barClicked)) {
+					options.barClicked.call(this, selectedTime);
+				}
+			});
+
+			$(this).on("click", '.pointer', function () {
+				pointerClicked();
+			});
+
+			$(this).on('click', '#deleteCuePoint', function () {
+				removeCuepoint();
+			});
+
+			$(this).on('click', '#addCuePoint', function () {
+				addCuePoint();
+			});
+
+			$(this).on('click', '#UpdateCuePoint', function () {
+				updateCuePoint();
+			});
+		});
+	};
+
+	$.fn.timebar.defaults = {
+		//properties
+		element: null,
+		totalTime: 0,
+		cuePoints: [],
+		width: 0,
+		globalPageX: 0,
+		selectedTime: 0,
+		// events
+		barClicked: null
+	};
+
+	function init(ele) {
+
+		const options = $.fn.timebar.defaults;
+
+		let data = '';
+
+		//time bar
+		data += `<div class='timeline-cover'>
+					<div id='draggable'></div>
+					<div class='timeline-bar'>
+						<div class='steps-bar clearfix'></div>
+					</div>
+				</div>`;
+
+		// buttons
+		data += `<div>
+					<button class='btn btn-primary' id='addCuePoint'>Add</button>
+					<button class='btn btn-primary' id='UpdateCuePoint'>Update</button>
+					<button class='btn btn-danger' id='deleteCuePoint'>Remove</button>
+				</div>`;
+
+		$(options.element).append(data);
+
+		const barWidth = $.fn.timebar.defaults.width;
+
+		ele.setWidth(barWidth);
+
+		const intervals = [{
+			position: 1,
+			time: '00:00'
+		}, {
+			position: 10,
+			time: toDuration(Math.round(options.totalTime / 5))
+		}, {
+			position: 20,
+			time: toDuration(Math.round((options.totalTime / 5) * 2))
+		}, {
+			position: 30,
+			time: toDuration(Math.round((options.totalTime / 5) * 3))
+		}, {
+			position: 40,
+			time: toDuration(Math.round((options.totalTime / 5) * 4))
+		}, {
+			position: 50,
+			time: toDuration(Math.round((options.totalTime / 5) * 5))
+		}];
+
+		// mark bars
+		for (let i = 1; i <= 50; i++) {
+			$(".steps-bar").append('<div class="step"><span class="step-border"></span></div>');
+		}
+
+		// mark time intervals
+		intervals.forEach((interval) => {
+			$(`.step:nth-child(${interval.position})`).append(`<span class="time-instant">${interval.time}</span>`);
+		});
+
+		if (options.cuePoints.length) {
+			markCuePoints(ele);
+		}
+	};
+
+	function toDuration(sec_num) {
+		let hours = Math.floor(sec_num / 3600);
+		let minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+		let seconds = sec_num - (hours * 3600) - (minutes * 60);
+		if (hours < 10) {
+			hours = "0" + Math.round(hours);
+		}
+		if (minutes < 10) {
+			minutes = "0" + Math.round(minutes);
+		}
+		if (seconds < 10) {
+			seconds = "0" + Math.round(seconds);
+		}
+		const time = (hours == 00) ? minutes + ':' + seconds : hours + ':' + minutes + ':' + seconds;
+		return time;
+	}
+
+	function markCuePoints(ele) {
+		const options = $.fn.timebar.defaults;
+
+		$.each(options.cuePoints, function (i, cuePoint) {
+			const eff = (cuePoint * 100) / options.totalTime;
+			const animateLeft = (ele.getActualWidth() * eff) / 100;
+			const divId = cuePoint;
+			$(".timeline-bar").append(`<div class="pointer" style="left:'${animateLeft} px'" id="${divId}"></div>`);
+		});
+	}
+
+	function barClicked(element, event, self) {
+		const options = $.fn.timebar.defaults;
+
+		const offset = $(element).offset();
+		const offsetLeft = (event.pageX - offset.left);
+
+		const leftPos = offsetLeft;
+		const leftPosCal = +(((leftPos * 100) / self.getActualWidth() + 2).toFixed(0));
+
+		options.selectedTime = (options.totalTime * leftPosCal) / 100;
+
+		$('.pointer').removeClass("pointerSelected");
+
+		$("#draggable").css({
+			left: offsetLeft
+		});
+
+		return options.selectedTime;
+	};
+
+	function pointerClicked() {
+		if ($(this).hasClass("pointerSelected")) {
+			$('.pointer').removeClass("pointerSelected");
+		} else {
+			$('.pointer').removeClass("pointerSelected");
+			$(this).addClass("pointerSelected");
+			let time = $(this).attr("id");
+		}
+	};
+
+	function addCuePoint() {
+		var cuePoint = data.response.cuePoint
+		var eff = ((cuePoint.startTime / 1000) * 100) / duration;
+		var animateLeft = (barLength * eff) / 100;
+		var divId = cuePoint.startTime;
+		$(".timeline-bar").append('<div class="pointer" _id="' + cuePoint._id + '" videoId= "' + cuePoint.videoId + '" media-id="' + cuePoint.mediaEntryId + '"  style="left:' + (animateLeft) + 'px" id="' + divId + '"></div>');
+		cuePoints.push(cuePoint);
+	};
+
+	function updateCuepoint() {
+		// var sourceContainer = $("#video-dropdown").val();
+		// var removeId = $(".pointerSelected").attr("_id");
+		const eff = ((temp.startTime / 1000) * 100) / duration;
+		const animateLeft = ($.fn.timebar.getActualWidth() * eff) / 100;
+		const divId = temp.startTime;
+		$(".timeline-bar").append('<div class="pointer" _id="' + temp.cuePointId + '" videoId= "' + temp.videoId + '" media-id="' + temp.mediaEntryId + '"  style="left:' + (animateLeft) + 'px" id="' + divId + '"></div>');
+		$(".pointerSelected").hide();
+		$("#UpdateCuePoint").hide();
+		$("#addCuePoint").show();
+		// removeFromList(temp.cuePointId);
+		// cuePoints.push(temp);
+	};
+
+	function removeCuepoint() {
+		// var removeId = $(".pointerSelected").attr("_id");
+		// var removeMediaId = $(".pointerSelected").attr("media-id");
+		$(".pointerSelected").hide();
+		// $("#startAt:text").val();
+		// removeFromList(removeId);
+		// clearBox();
+	};
+
+})(jQuery);
